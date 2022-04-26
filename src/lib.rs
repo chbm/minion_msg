7f9c8ddbe5eadb3f51e177a24d7a225523d2658f
Carlos Morgado <carlos primatas.org>
2022-04-27 00:10:58 +0100

    uuids everywhere

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,33 @@
 use num_enum::{IntoPrimitive, TryFromPrimitive, UnsafeFromPrimitive};
 use serde::{Serialize, Deserialize};
 use postcard;
+use uuid::{uuid, Uuid};
+
+pub struct MinionId(Uuid);
+
+impl MinionId {
+    pub fn new() -> MinionId {
+        MinionId(Uuid::new_v4())
+    }
+
+    pub fn nil() -> MinionId {
+        MinionId(Uuid::nil())
+    }
+}
+
+impl From<Vec<u8>> for MinionId {
+    fn from(v: Vec<u8>) -> MinionId {
+        MinionId(Uuid::from_slice(&v).unwrap())
+    }
+}
+
+impl From<MinionId> for Vec<u8> {
+    fn from(i: MinionId) -> Vec<u8> {
+        match i { MinionId(u) => u }.as_bytes().to_vec()
+    }
+}
+
+
 
 #[derive(Deserialize, IntoPrimitive, Serialize, TryFromPrimitive, UnsafeFromPrimitive, Debug)]
 #[repr(u8)]
